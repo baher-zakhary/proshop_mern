afddb06914ba06e13c424d3ddaead8f76612a3e6
Baher Zakhary <baher.zakharyy@gmail.com>
2022-03-30 22:26:24 +0200

    - Setup react router v6

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
 import Rating from './Rating'
+import { Link } from 'react-router-dom'
 
-const Product = ({product}) => {
+const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
+            <Link to={`/product/${product._id}`}>
                 <Card.Img src={product.image} variant='top' />
-            </a>
+            </Link>
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <Link to={`/product/${product._id}`}>
                     <Card.Title as='div'>
                         <strong>{product.name}</strong>
                     </Card.Title>
-                </a>
+                </Link>
 
                 <Card.Text as='div'>
                     <Rating
@@ -23,7 +24,7 @@ const Product = ({product}) => {
                 </Card.Text>
 
                 <Card.Text as='h3'>
-                    ${product.price}     
+                    ${product.price}
                 </Card.Text>
             </Card.Body>
         </Card>
