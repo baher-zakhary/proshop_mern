06ccabc86981cf76d0bbf1ccd90c091b932722a2
Baher Zakhary <baher.zakhary@idemia.com>
2022-03-30 14:31:42 +0200

    - Add react bootstrap - Use bootswatch theme - Add Header component - Add Footer component - Add images & products static JSON data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { Container } from "react-bootstrap"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
-  return (
-    <>
-      <h1>Welcome To ProShop</h1>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <main className="py-3">
+                <Container>
+                    <h1>Welcome To ProShop</h1>
+                </Container>
+            </main>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
