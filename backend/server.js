87aa1c89002612d4adca6ee6acc88e9eb80ae36c
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-01 17:49:15 +0200

    - Use dotenv to store and read environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
+const dotenv = require('dotenv')
 const express = require('express')
 const products = require('./data/products')
+
+dotenv.config()
 
 const app = express()
 
@@ -16,4 +19,9 @@ app.get('/v1/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
