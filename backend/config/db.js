0d564a265907f9d90386e25fe42c5abe788a52b5
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-02 17:41:33 +0200

    - Remove useNewUrlParser, useUnifiedTopology and useCreateIndex as they are not supported in mongoose v6

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,11 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true
-        })
+        const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`Mongo connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(`Error: ${error.message}`)
