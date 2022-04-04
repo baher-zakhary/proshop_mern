afb2202c008b2a7c50ff5d43fee82b4f09b335c4
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-04 08:25:34 +0200

    - Use redux to load products and handle errors if any

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,38 @@
-import React, { useState, useEffect} from 'react'
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product'
-import axios from 'axios'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import { listProducts } from "../actions/productActions";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/v1/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+  const productList = useSelector((state) => state.productList);
 
-    return (
-        <>
-            <h1>Latest Products</h1>
-            <Row>
-                {products.map(product => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
-}
+  const { loading, products, error } = productList;
 
-export default HomeScreen
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      {loading ? (
+        <h2>loading ...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
+
+export default HomeScreen;
