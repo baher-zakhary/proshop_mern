e7a07e052086cdc94e1100e6b3a523fdf10bb684
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-11 07:07:33 +0200

    - Add v1

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,7 +2,7 @@ import Product from '../models/productModel.js'
 import asyncHandler from "express-async-handler";
 
 // @desc    Fetch all products
-// @route   GET /api/products
+// @route   GET v1/api/products
 // @access  Public
 export const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({})
@@ -10,7 +10,7 @@ export const getProducts = asyncHandler(async (req, res) => {
 })
 
 // @desc    Fetch a product by Id
-// @route   GET /api/products/:id
+// @route   GET v1/api/products/:id
 // @access  Public
 export const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
