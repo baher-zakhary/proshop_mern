f1079adb1a4176acea18f62f3406a86b515bfc9c
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-12 08:37:56 +0200

    - Refactor use User Dto

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import { generateToken } from '../utils/authUtils.js'
+import { UserDto } from '../dtos/UserDto.js';
 
 // @desc    Auth user & get token
 // @route   POST v1/api/login
@@ -9,13 +10,9 @@ export const authUser = asyncHandler(async (req, res) => {
     const { email, password } = req.body
     const user = await User.findOne({ email })
     if (user && await user.matchPassword(password)) {
-        res.json({
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            token: generateToken(user._id)
-        })
+        const userDto = new UserDto(user)
+        userDto.token = generateToken(user._id)
+        res.json(userDto)
     } else {
         res.status(401)
         throw new Error('Invalid email or password')
