b56c75620877f5d298306aaf641c7e4343b512e6
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-13 06:44:05 +0200

    - Hash password pre save middleware - Register user controller method - Setup routing to POST users

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 import asyncHandler from 'express-async-handler'
 import { UserDto } from '../dtos/UserDto.js';
 import User from '../models/userModel.js'
+import { generateToken } from '../utils/authUtils.js';
 
 // @desc    Get user profile
 // @route   GET v1/api/users/profile
@@ -13,5 +14,36 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     } else {
         res.status(404)
         throw new Error('User not found')
+    }
+})
+
+// @desc    Register a new user
+// @route   POST /v1/api/users
+// @access  Public
+export const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    const userExists = await User.findOne({ email })
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        const userDto = new UserDto(user)
+        res.status(201).json({
+            ...userDto,
+            token: generateToken(userId)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
     }
 })
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getUserProfile } from '../controllers/userController.js'
+import { getUserProfile, registerUser } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+router.route('/').post(registerUser)
 router.route('/profile').get(protect, getUserProfile)
 
 export default router
