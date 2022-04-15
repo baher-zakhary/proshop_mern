04a1f5f723dc2575c697cd111014ea1b245acc4b
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-15 14:58:06 +0200

    - Position loader in the center of the screen - Display a transparent background overlay behind the loader

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,10 +3,13 @@ import { Spinner } from 'react-bootstrap'
 
 const Loader = () => {
   return (
-    <Spinner animation='border' role='status'
-        style={{width: '100px', height: '100px', margin: 'auto', display: 'block'}}>
-            <span className='sr-only'>Loading...</span>
-    </Spinner>
+    <>
+      <div style={{height: '100%', width: '100%', position: 'fixed', top: '0', left: '0', backgroundColor: 'white', opacity: '0.4'}}></div>
+      <Spinner animation='border' role='status'
+          style={{width: '100px', height: '100px', margin: '-50px 0 0 -50px', position: 'absolute', top: '50%', left: '50%'}}>
+              <span className='sr-only'>Loading...</span>
+      </Spinner>
+    </>
   )
 }
 
