9b768668696c2b7298a16814595bba3af0d0d197
Baher Zakhary <baher.zakharyy@gmail.com>
2022-04-15 14:58:28 +0200

    - Fix login route

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -8,7 +8,7 @@ export const login = (email, password) => async (dispatch) => {
     });
 
     const config = { headers: { "Content-Type": "application/json" } };
-    const { data } = await axios.post("/api/users/login", { email, password }, config);
+    const { data } = await axios.post("/v1/api/auth/login", { email, password }, config);
 
     dispatch({
       type: userActionTypes.USER_LOGIN_SUCCESS,
