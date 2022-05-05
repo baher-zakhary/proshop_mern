f720bbdbe320b77df5d1189d2a19f71431ddbb84
Baher Zakhary <baher.zakharyy@gmail.com>
2022-05-05 17:35:41 +0200

    - Reset redux and local storage on logout

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,51 +1,84 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-import { productListReducer, productDetailsReducer } from './reducers/productReducers'
-import { cartReducer } from './reducers/cartReducers'
-import { userDetailsReducer, userLoginReducer, userRegisterReducer } from './reducers/userReducers'
-import { orderCreateReducer, orderDetailsReducer, orderPayReducer } from './reducers/orderReducers'
-import { localStorageKeys } from './constants/localStorageKeys'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  userDetailsReducer,
+  userLoginReducer,
+  userRegisterReducer,
+} from "./reducers/userReducers";
+import {
+  listMyOrdersReducer,
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+} from "./reducers/orderReducers";
+import { localStorageKeys } from "./constants/localStorageKeys";
+import { userActionTypes } from "./constants/actionTypes/userActionTypes";
 
-const reducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer,
-    cart: cartReducer,
-    userLogin: userLoginReducer,
-    userRegister: userRegisterReducer,
-    userDetails: userDetailsReducer,
-    orderCreate: orderCreateReducer,
-    orderDetails: orderDetailsReducer,
-    orderPay: orderPayReducer
-})
+const appReducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  listMyOrders: listMyOrdersReducer,
+});
+
+const rootReducer = (state, action) => {
+  if (action.type === userActionTypes.userLogin.USER_LOGOUT) {
+    // Clear local storage on logout
+    localStorage.clear();
+    // reset all redux states on logout
+    return appReducer(undefined, action);
+  }
+
+  return appReducer(state, action);
+};
 
 const cartItemsFromStorage = localStorage.getItem(localStorageKeys.CART_ITEMS)
-        ? JSON.parse(localStorage.getItem(localStorageKeys.CART_ITEMS))
-        : []
+  ? JSON.parse(localStorage.getItem(localStorageKeys.CART_ITEMS))
+  : [];
 const userInfoFromStorage = localStorage.getItem(localStorageKeys.USER_INFO)
-        ? JSON.parse(localStorage.getItem(localStorageKeys.USER_INFO))
-        : null
-const shippingAddressFromStorage = localStorage.getItem(localStorageKeys.SHIPPING_ADDRESS)
-        ? JSON.parse(localStorage.getItem(localStorageKeys.SHIPPING_ADDRESS))
-        : {}
-const paymentMethodFromStorage = localStorage.getItem(localStorageKeys.PAYMENT_METHOD)
-        ? JSON.parse(localStorage.getItem(localStorageKeys.PAYMENT_METHOD))
-        : null
+  ? JSON.parse(localStorage.getItem(localStorageKeys.USER_INFO))
+  : null;
+const shippingAddressFromStorage = localStorage.getItem(
+  localStorageKeys.SHIPPING_ADDRESS
+)
+  ? JSON.parse(localStorage.getItem(localStorageKeys.SHIPPING_ADDRESS))
+  : {};
+const paymentMethodFromStorage = localStorage.getItem(
+  localStorageKeys.PAYMENT_METHOD
+)
+  ? JSON.parse(localStorage.getItem(localStorageKeys.PAYMENT_METHOD))
+  : null;
 
 const initialState = {
-    cart: {
-        cartItems: cartItemsFromStorage,
-        shippingAddress: shippingAddressFromStorage,
-        paymentMethod: paymentMethodFromStorage
-    },
-    userLogin: {
-        userInfo: userInfoFromStorage
-    }
-}
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+    paymentMethod: paymentMethodFromStorage,
+  },
+  userLogin: {
+    userInfo: userInfoFromStorage,
+  },
+};
 
-const middleware = [thunk]
+const middleware = [thunk];
 
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
-export default store; 
+export default store;
