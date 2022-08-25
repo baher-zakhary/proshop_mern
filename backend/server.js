34d6bd50184fe5f16d6f227e339678dfc8de2827
Baher Zakhary <baher.zakharyy@gmail.com>
2022-08-25 14:36:59 +0200

    - Add morgan middleware for logging requests

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import path from 'path'
-
+import morgan from 'morgan';
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
@@ -17,6 +17,10 @@ dotenv.config()
 connectDB()
 
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
