f232902c5dea63a996509e8304f3e3089e652eba
Baher Zakhary <baher.zakharyy@gmail.com>
2022-08-25 17:55:13 +0200

    - Feat: Create product review [backend]

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -82,3 +82,41 @@ export const updateProduct = asyncHandler(async (req, res) => {
         throw new Error('Product not found')
     }
 });
+
+// @desc    Create a new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+export const createProductReview = asyncHandler(async (req, res) => {
+    const {rating, comment} = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+        const alreadyReviewed = product.reviews.find(
+            review => review.user.toString() === req.userId.toString()
+        );
+
+        if (alreadyReviewed) {
+            res.status(400);
+            throw new Error('Product already reviewed');
+        }
+
+        const user = User.findById(req.userId);
+        const review = {
+            name: user.name,
+            rating: Number(rating),
+            comment,
+            user: req.userId
+        }
+
+        product.reviews.push(review);
+        product.numReviews = product.reviews.length;
+        product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / products.reviews.length;
+
+        await product.save();
+        res.status(201).json({ message: 'Review added' })
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+});
