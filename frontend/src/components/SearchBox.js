27d2c116e182db49b64f64e83dc9f2cdc297fc61
Baher Zakhary <baher.zakharyy@gmail.com>
2022-08-28 16:28:47 +0200

    - Feat: Change search button color

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -35,7 +35,7 @@ const SearchBox = () => {
         placholder='Search ...'
         className='mr-sm-2 ml-sm-5'
       ></Form.Control>
-      <Button type='submit' variant='outline-success' className="mt-0 p-2">
+      <Button type='submit' variant='outline-info' className="mt-0 p-2">
         Search
       </Button>
     </Form>
